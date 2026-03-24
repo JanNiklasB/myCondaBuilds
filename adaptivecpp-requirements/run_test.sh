af2d56561672b6dc83f105c50b43d6745a9347fb
Jan-Niklas Bohnensack <jan-niklas.bohnensack@rub.de>
2026-03-24 17:25:15 +0100

    removed boost, fixed tests

--- adaptivecpp-requirements/run_test.sh
+++ adaptivecpp-requirements/run_test.sh
@@ -1,13 +1,14 @@
 set -ex
+echo $PREFIX
 
 cd AdaptiveCpp
 mkdir build && cd build
-cmake .. -G Ninja -DCMAKE_PREFIX_PATH=$TEST_PREFIX -DCMAKE_INSTALL_PREFIX=$TEST_PREFIX -DROCM_DEVICE_LIBS_PATH=$TEST_PREFIX/lib/amdgcn/bitcode/
+cmake .. -G Ninja -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX -DROCM_DEVICE_LIBS_PATH=$PREFIX/lib/amdgcn/bitcode/
 cmake --build .
 cmake --install .
 
-cd ../../TestFiles
+cd TestFiles
 mkdir build && cd build
-cmake .. -G Ninja -DCMAKE_PREFIX_PATH=$TEST_PREFIX -DCMAKE_INSTALL_PREFIX=$TEST_PREFIX
+cmake .. -G Ninja -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
 cmake --build .
 ./VectorAdd
