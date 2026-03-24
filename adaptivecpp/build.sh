1a2acb152115b11b4a113a52d7ee9a427623f402
Jan-Niklas Bohnensack <jan-niklas.bohnensack@rub.de>
2026-03-24 10:34:46 +0100

    now using cp instead of ln

--- adaptivecpp/build.sh
+++ adaptivecpp/build.sh
@@ -14,8 +14,8 @@ mkdir build && cd build
 cmake .. -G Ninja -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 cmake --install .
-ln -s $PREFIX/lib/libLLVM-19.so $PREFIX/lib/libLLVM.so
-# cp $PREFIX/lib/libLLVM-19.so $PREFIX/lib/libLLVM.so
+# ln -s $PREFIX/lib/libLLVM-19.so $PREFIX/lib/libLLVM.so
+cp $PREFIX/lib/libLLVM-19.so $PREFIX/lib/libLLVM.so
 
 # clang:
 cd $SRC_DIR/llvm-project/clang/
