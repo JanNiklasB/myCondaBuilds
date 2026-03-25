79b4cd41c80ef8bf8efc0e4ab038ca822b17b732
Jan-Niklas Bohnensack <jan-niklas.bohnensack@rub.de>
2026-03-25 05:11:43 +0100

    added new adaptivecpp-requirements package to meta.yaml

--- adaptivecpp/run_test.sh
+++ adaptivecpp/run_test.sh
@@ -2,7 +2,6 @@ set -ex
 
 cd TestFiles
 mkdir build && cd build
-cmake .. -G Ninja -DCMAKE_PREFIX_PATH=$TEST_PREFIX
+cmake .. -G Ninja -DCMAKE_PREFIX_PATH=$PREFIX
 cmake --build .
 ./VectorAdd
-ldd VectorAdd
